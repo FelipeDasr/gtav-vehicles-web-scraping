fbb9e6f9a1f9a1c9f276d2c5753198ddb2ac0c82
FelipeDasr <felipe.heiker07@gmail.com>
2023-02-13 23:56:32 -0400

    feat: scrapers handler

---
+++ src/scraper.ts
@@ -0,0 +1,35 @@
+import { IWikiRaceScraper } from './web-sources/wiki-rage';
+import puppeteer, { Browser } from 'puppeteer';
+import { IWebSource } from './dtos/webSources';
+import { VehicleType } from './dtos/vehicle';
+
+export class Scraper {
+    private browser: Browser;
+
+    public async init() {
+        await this.setBrowser();
+    }
+
+    private async setBrowser() {
+        this.browser = await puppeteer.launch({
+            headless: false,
+            timeout: 10000,
+        });
+    }
+
+    private async getWebSource(source: IWebSource) {
+        switch (source) {
+            case 'wiki-rage':
+                return new IWikiRaceScraper();
+        }
+    }
+
+    public async closeBrowser() {
+        await this.browser.close();
+    }
+
+    public async start(sourceName: IWebSource, exclude?: VehicleType[]) {
+        const source = await this.getWebSource(sourceName);
+        return source.scrape(this.browser, exclude);
+    }
+}
