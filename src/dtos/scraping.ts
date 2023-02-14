be4c4395f9894741826b663bb93d61d50b55a3bf
FelipeDasr <felipe.heiker07@gmail.com>
2023-02-13 23:54:26 -0400

    refact: update scraping interfaces

--- src/dtos/scraping.ts
+++ src/dtos/scraping.ts
@@ -1,5 +1,7 @@
 import { IVehicle, VehicleType } from './vehicle';
+import { Browser } from 'puppeteer';
 
-export type IScrapingResponse = Partial<
-  { [Key in keyof VehicleType]: IVehicle }[keyof VehicleType]
->;
+export type IScrapingResponse = Partial<{ [Key in VehicleType]: IVehicle[] }>;
+export interface IGtavVehicleScraper {
+    scrape(browser: Browser, exclude?: VehicleType[]): Promise<IScrapingResponse>;
+}
